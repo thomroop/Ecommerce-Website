1699d852fc8b8e8b32973a7bb2a7ef02c31251bb
thomroop <thomas.roopa@gmail.com>
2025-08-20 10:43:17 +0530

    files updated

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,1 +1,14 @@
+ const mongoose = require("mongoose");
+ const connectDB = async() => {
+
+    try{
+        await mongoose.connect("mongodb://localhost:27017/Ecommerce")
+        console.log("Mongo DB connected successfully")
+    }catch(error){
+        console.log("MongoDB connection failed")
+    }
+    
+ }
+
+ module.exports = connectDB;
  
--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,13 @@
  
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+app.use(express.json());
+
+connectDB();
+
+app.listen(8000,() => {
+    console.log ("server running on 8000")
+})
